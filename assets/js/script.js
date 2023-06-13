dfdd8f3e68d1047f6008bcd199544e07549f2a0d
Retr01234 <>
2023-06-13 09:58:22 +0000

    feature: create addition section

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,16 +10,25 @@ document.addEventListener("DOMContentLoaded", function () {
                 alert("You clicked Submit");
             } else {
                 let gameType = this.getAttribute("data-type");
-                alert(`You clicked ${gameType}`);
+                runGame(gameType);
             }
         });
     }
+
+    runGame("addition");
 });
 
-function runGame() {
+function runGame(gameType) {
     // Generates 2 Random Numbers betwwen 1 and 24
     let num1 = Math.floor(Math.random() * 25) + 1;
     let num2 = Math.floor(Math.random() * 25) + 1;
+
+    if (gameType === "addition") {
+        displayAdditionQuestion(num1, num2);
+    } else {
+        alert(`Unknown Game Type: ${gameType}`);
+        throw `Unknown Game Type: ${gameType}. Aborting!`;
+    }
 }
 
 function checkAnswer() {
@@ -38,8 +47,10 @@ function incrementWrongAnswer() {
 
 }
 
-function displayAdditionQuestion() {
-
+function displayAdditionQuestion(operand1, operand2) {
+    document.getElementById("operand1").textContent = operand1;
+    document.getElementById("operand2").textContent = operand2;
+    document.getElementById("operator").textContent = "+";
 }
 
 function displaySubtractQuestion() {
